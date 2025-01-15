5077b53c7ad8a4bb1fd0e3f6ac4ec9e282c122e1
katatonia <pmilekhina@gmail.com>
2025-01-15 19:26:06 +0300

    feature: адаптив на 1024

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -2,17 +2,13 @@ export const accordion = () => {
 	document.querySelectorAll('.accordion__header').forEach(header => {
 		header.addEventListener('click', () => {
 			const content = header.nextElementSibling;
-			// const icon = header.querySelector('.icon');
 
 			if (content.classList.contains('open')) {
 				content.classList.remove('open');
-				// icon.classList.remove('open');
 			} else {
 				document.querySelectorAll('.accordion__content').forEach(c => c.classList.remove('open'));
 				document.querySelectorAll('.icon').forEach(i => i.classList.remove('open'));
-
 				content.classList.add('open');
-				// icon.classList.add('open');
 			}
 		});
 	});
